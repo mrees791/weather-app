a5c2e09f21a8e15b66fcf92bcb47842c9add6064
Michael Rees <mrees791@gmail.com>
2020-12-26 17:00:22 -0500

    Created ConsoleWeatherTest module for additional testing.

---
+++ ConsoleWeatherTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherLibrary.Models.OpenWeatherMap;
+
+namespace ConsoleWeatherTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+
+        public void Start()
+        {
+            var oneCallRequest = new OneCallRequest();
+
+            oneCallRequest.RequestOneCall(33.441792, -94.037689);
+
+            if (oneCallRequest.IsValidRequest)
+            {
+                Console.WriteLine("Hourly");
+                for (int i = 0; i < 5; i++)
+                {
+                    var hourly = oneCallRequest.OneCall.HourlyEntries[i];
+                    Console.WriteLine($"{hourly.DateTime.ToShortTimeString()} - {hourly.TemperatureCelsius}C");
+                }
+                Console.WriteLine("\nDaily");
+                for (int i = 0; i < 7; i++)
+                {
+                    var daily = oneCallRequest.OneCall.DailyEntries[i];
+                    Console.WriteLine($"{daily.DateTime.ToShortDateString()} - High: {daily.DailyTemperature.DailyHighCelsius}C Low: {daily.DailyTemperature.DailyLowCelsius}C {daily.WeatherEntries[0].Description}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error sending one call request.");
+            }
+        }
+    }
+}
