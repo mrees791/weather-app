3150276f5f95c6e50cbd1e5c746c406de789a4df
Michael Rees <mrees791@gmail.com>
2021-01-02 16:53:30 -0500

    Added all weather types for WeatherType enum from OpenWeatherMap.

--- WeatherLibrary/Models/WeatherType.cs
+++ WeatherLibrary/Models/WeatherType.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace WeatherLibrary.Models
 {
+    /// <summary>
+    /// Weather types from OpenWeatherMap
+    /// https://openweathermap.org/weather-conditions
+    /// </summary>
     public enum WeatherType
     {
-        Clear, Cloudy, Rain, Snow, Unknown
+        Thunderstorm, Drizzle, Rain, Snow,
+        Mist, Smoke, Haze, Dust, Fog, Sand,
+        Ash, Squall, Tornado, Clear, Cloudy,
+        Unknown
     }
 }
