dc34484fc4346f2bcdfa71dc26d0b29c175ce767
Michael Rees <mrees791@gmail.com>
2021-01-04 18:04:46 -0500

    Added decimals to variables in unit tests.

--- WeatherTests/WeatherTests.cs
+++ WeatherTests/WeatherTests.cs
@@ -15,7 +15,7 @@ namespace WeatherTests
         public void TestFahrenheitTemperatures()
         {
             Temperature freezing = new Temperature(32.0, TemperatureFormat.Fahrenheit);
-            Temperature boiling = new Temperature(212, TemperatureFormat.Fahrenheit);
+            Temperature boiling = new Temperature(212.0, TemperatureFormat.Fahrenheit);
 
             Assert.AreEqual(0.0, freezing.Celsius, 0.01, "Freezing's celsius temperature should be 0.");
             Assert.AreEqual(100.0, boiling.Celsius, 0.01, "Boiling's celsius temperature should be 100.");
@@ -28,8 +28,8 @@ namespace WeatherTests
             Temperature freezing = new Temperature(0.0, TemperatureFormat.Celsius);
             Temperature boiling = new Temperature(100.0, TemperatureFormat.Celsius);
 
-            Assert.AreEqual(32, freezing.Fahrenheit, 0.01, "Freezing's fahrenheit temperature should be 32.");
-            Assert.AreEqual(212, boiling.Fahrenheit, 0.01, "Boiling's fahrenheit temperature should be 212.");
+            Assert.AreEqual(32.0, freezing.Fahrenheit, 0.01, "Freezing's fahrenheit temperature should be 32.");
+            Assert.AreEqual(212.0, boiling.Fahrenheit, 0.01, "Boiling's fahrenheit temperature should be 212.");
         }
     }
 }
