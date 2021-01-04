147c02a772b8a2296f1bb38e69d911d2591a8be9
Michael Rees <mrees791@gmail.com>
2021-01-04 18:02:29 -0500

    Added unit tests for Temperature model.

---
+++ WeatherTests/WeatherTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherLibrary.Models;
+
+namespace WeatherTests
+{
+    [TestClass]
+    public class WeatherTests
+    {
+        [TestMethod]
+        public void TestFahrenheitTemperatures()
+        {
+            Temperature freezing = new Temperature(32.0, TemperatureFormat.Fahrenheit);
+            Temperature boiling = new Temperature(212, TemperatureFormat.Fahrenheit);
+
+            Assert.AreEqual(0.0, freezing.Celsius, 0.01, "Freezing's celsius temperature should be 0.");
+            Assert.AreEqual(100.0, boiling.Celsius, 0.01, "Boiling's celsius temperature should be 100.");
+
+        }
+
+        [TestMethod]
+        public void TestCelsiusTemperatures()
+        {
+            Temperature freezing = new Temperature(0.0, TemperatureFormat.Celsius);
+            Temperature boiling = new Temperature(100.0, TemperatureFormat.Celsius);
+
+            Assert.AreEqual(32, freezing.Fahrenheit, 0.01, "Freezing's fahrenheit temperature should be 32.");
+            Assert.AreEqual(212, boiling.Fahrenheit, 0.01, "Boiling's fahrenheit temperature should be 212.");
+        }
+    }
+}
